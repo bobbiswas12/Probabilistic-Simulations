53225acf0a29a2f66ba69934868579e0c05bf615
bobbiswas12 <tanmayrai1729@gmail.com>
2026-06-08 18:15:14 +0530

    random walk

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 build:
 	gcc pi/pi.c -o Pi -lraylib -lm -lpthread -ldl -lrt
+	gcc random_walks/main.c -o random_walk -lraylib -lm -lpthread -ldl -lrt
---
+++ random_walks/main.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+#include "raylib.h"
+
+#define screen_width 1366
+#define screen_height 768
+#define fps 10
+#define grid_x 40
+#define grid_y 40
+#define points_y (float) screen_height / grid_y
+#define points_x (float) (screen_width - 120) / grid_x
+
+float random_value(float value){
+
+  return (((float) rand()/(float) RAND_MAX)*(float) value);
+}
+
+void draw_graph(float screenw, float screenh) {
+
+  Vector2 y_pos = (Vector2){100, 0};
+  Vector2 y_neg = (Vector2){100, screenh};
+  Vector2 x_neg = (Vector2){0, screenh / 2};
+  Vector2 x_pos = (Vector2){screenw, screenh / 2};
+
+  DrawLineV(y_pos, y_neg, WHITE);
+  DrawLineV(x_pos, x_neg, WHITE);
+
+  for (int i = -200; i <= grid_x; i++) {
+
+    if (i!=0){
+
+      Vector2 y_pos = (Vector2){100 + points_x * i, 0};
+      Vector2 y_neg = (Vector2){100 + points_x * i, screenh};
+      DrawLineV(y_pos, y_neg, DARKGRAY);
+    }
+  }
+
+  for (int i = 1; i <= grid_y/2; i++) {
+
+    if (i != 0) {
+      
+      Vector2 x_pos = (Vector2){0,screenh/2 - points_y * i };
+      Vector2 x_neg = (Vector2){screenw, screenh/2 - points_y * i};
+      DrawLineV(x_pos, x_neg, DARKGRAY);
+
+      Vector2 x_pos_1 = (Vector2){0,screenh/2 + points_y * i };
+      Vector2 x_neg_1 = (Vector2){screenw, screenh / 2 + points_y * i};
+      DrawLineV(x_pos_1,x_neg_1,DARKGRAY);
+    }
+  }
+}
+
+void transform_point(Vector2 *point, Vector2 origin) {
+  point->x += origin.x + point->x * points_x;
+  point->y += origin.y - point->y * points_y;
+}
+
+void draw_point(Vector2 position_point,Vector2 origin) {
+
+  /* Vector2 new_pos = (Vector2) {origin.x + points_x *
+   * position_point.x,origin.y - points_y * position_point.y}; */
+  transform_point(&position_point,origin);
+  DrawCircleV(position_point, 5, DARKBLUE);
+}
+
+void path_from_to(Vector2 initial_point, Vector2 final_point) {
+
+  DrawCircleV(final_point, 5, DARKBLUE);
+  DrawLineV(initial_point, final_point, GREEN);
+}
+
+void update(Vector2 *point, Vector2 origin, int dir) {
+
+  if (dir == 0) {
+    point->x = point->x + points_x;
+    point->y = point->y + points_y;
+  } else {
+    point->x = point->x + points_x;
+    point->y = point->y - points_y;
+  }
+}
+  
+
+int main(){
+
+
+  InitWindow(screen_width,screen_height,"Random Walk");
+  SetTargetFPS(fps);
+  Vector2 origin = (Vector2){100, (float)screen_height / 2};
+
+  Vector2 point_start;
+  int steps;
+  printf("Starting Point:");
+  scanf("%f %f", &point_start.x, &point_start.y);
+  printf("No. of Steps: ");
+  scanf("%d",&steps);
+
+  int step_counter = 0;
+  transform_point(&point_start,origin);
+  
+  while (!WindowShouldClose()){
+
+    BeginDrawing();
+    ClearBackground(BLACK);
+
+    draw_graph(screen_width, screen_height);
+    draw_point(point_start, origin);
+
+    if (step_counter <= steps) {
+      
+      int a = (int) random_value(2);
+      Vector2 prev_point = point_start;
+      update(&point_start, origin, a);
+      path_from_to(prev_point, point_start);
+    }
+    step_counter++;
+    DrawFPS(10,10);
+    EndDrawing();
+  }
+  CloseWindow();
+  return 0;
+}
